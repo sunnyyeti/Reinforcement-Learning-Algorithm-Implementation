fea7f1697591040d877170ca085b982d2773a7a5
sunnyyeti <sunnyyetizh@gmail.com>
2018-12-20 00:07:00 +0100

    fff

--- Chapter2/the_10_armed_testbed.py
+++ Chapter2/the_10_armed_testbed.py
@@ -5,12 +5,12 @@ class Bandit():
     EPS_GREEDY = "__eps_greedy__"
     UCB = "__upper_confidence_bound__"
     GRADIENT = "__gradient_bandit__"
-    def __init__(self, K_arms, q_true_mean, q_true_var, arm_reward_var, q_estimation_initial, strategy, sample_average, step_size):
+    def __init__(self, k_arms, q_true_mean, q_true_std, arm_reward_std, q_estimation_initial, strategy, sample_average, step_size):
         """
         :param K_arms: number of arms in a bandit problem
         :param q_true_mean: double. The mean of the normal distribution used to sample true action values for each arm.
-        :param q_true_var: double. The variance of the normal distribution used to sample true action values for each arm.
-        :param arm_reward_var: double. The variance used to sample the actual reward at each step for each arm.
+        :param q_true_std: double. The standard deviation of the normal distribution used to sample true action values for each arm.
+        :param arm_reward_std: double. The standard deviation used to sample the actual reward at each step for each arm.
         :param q_estimation_initial: double. The initial estimation values for each arm.
         :param strategy: dict. Key "name" is required, the value could be one of {Bandit.GREEDY, Bandit.EPS_GREEDY, Bandit.UCB, Bandit.GRADIENT}
                                Other keys required:
@@ -21,10 +21,11 @@ class Bandit():
         :param sample_average: bool. Whether to update the action value estimations by sample averages. If True, step_size is ignored.
         :param step_size: double. Constant step_size for updating action value estimations.
         """
-        self.K_arms = K_arms
+        self.k_arms = k_arms
+        self.actions = np.arange(self.k_arms)
         self.q_true_mean = q_true_mean
-        self.q_true_var = q_true_var
-        self.arm_reward_var = arm_reward_var
+        self.q_true_std = q_true_std
+        self.arm_reward_std = arm_reward_std
         self.q_estimation_initial = q_estimation_initial
         self.strategy = strategy
         self.sample_average = sample_average
@@ -34,4 +35,27 @@ class Bandit():
         """
         Initialize a new bandit problem.
         """
+        self.q_true = self.q_true_std*np.random.randn(self.k_arms)+self.q_true_mean
+        self.q_estimation = np.zeros(self.k_arms,dtype=np.float64)+self.q_estimation_initial
+        self.action_count = np.zeros(self.k_arms,dtype=np.int32)
+        self.best_action = np.argmax(self.q_true)
+
+    def take_action(self):
+        if self.strategy["name"] == Bandit.GREEDY:
+            max_est = np.max(self.q_estimation)
+            action = np.random.choice([act for act, q_est in enumerate(self.q_estimation) if q_est==max_est])
+            return action
+        if self.strategy["name"] == Bandit.EPS_GREEDY:
+            epsilon = self.strategy.pop["epsilon"]
+            if np.random.rand()<epsilon:
+                action = np.random.choice(self.actions)
+                return action
+            else:
+                max_est = np.max(self.q_estimation)
+                action = np.random.choice([act for act, q_est in enumerate(self.q_estimation) if q_est == max_est])
+                return action
+        if self.strategy["name"] == Bandit.UCB:
+            
+
+
         
