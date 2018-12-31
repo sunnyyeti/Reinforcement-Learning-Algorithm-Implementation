75ca7d0c5556c43182797876a37ef943c7550000
sunnyyeti <sunnyyetizh@gmail.com>
2018-12-31 22:56:54 +0100

    rental car

--- Chapter4/Jack_car_rental.py
+++ Chapter4/Jack_car_rental.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
+
 class Poisson():
     CACHE = {}
 
@@ -47,33 +48,38 @@ class CarCompany():
         :param action: int. Number of cars moved from first location to second location.
         :return: float. Expected reward.
         """
-        first_loc, second_loc = state
-        first_loc = min(first_loc - action, CarCompany.MAX_CARS)
-        second_loc = min(second_loc + action, CarCompany.MAX_CARS)
+
         reward = -CarCompany.MOVE_COST * abs(action)
         for req1 in range(CarCompany.POISSON_UPBOUND + 1):
+            first_loc = state[0]
+            first_loc = min(first_loc - action, CarCompany.MAX_CARS)
+            real_req1 = min(req1, first_loc)
+            first_loc -= real_req1
             p1 = self.request1(req1)
+            accp1 = p1
             for req2 in range(CarCompany.POISSON_UPBOUND + 1):
+                second_loc = state[1]
+                second_loc = min(second_loc + action, CarCompany.MAX_CARS)
                 p2 = self.request2(req2)
-                real_req1 = min(req1, first_loc)
                 real_req2 = min(req2, second_loc)
-                first_loc -= real_req1
                 second_loc -= real_req2
                 benefits = CarCompany.RENTAL_FEE * (real_req1 + real_req2)
+                accp2 = accp1 * p2
                 for ret1 in range(CarCompany.POISSON_UPBOUND + 1):
                     p3 = self.return1(ret1)
+                    first_loc_ = min(first_loc + ret1, CarCompany.MAX_CARS)
+                    accp3 = accp2 * p3
                     for ret2 in range(CarCompany.POISSON_UPBOUND + 1):
                         p4 = self.return2(ret2)
-                        first_loc = min(first_loc + ret1, CarCompany.MAX_CARS)
-                        second_loc = min(second_loc + ret2, CarCompany.MAX_CARS)
-                        prob = p1 * p2 * p3 * p4
-                        reward += prob * (benefits + CarCompany.DISCOUNT * self.state_values[first_loc, second_loc])
+                        second_loc_ = min(second_loc + ret2, CarCompany.MAX_CARS)
+                        prob = accp3 * p4
+                        reward += prob * (benefits + CarCompany.DISCOUNT * self.state_values[first_loc_, second_loc_])
         return reward
 
-    def policy_evaluation(self, epsilon=1e-5):
+    def policy_evaluation(self, epsilon=1e-4):
         step = 0
         while True:
-            step+=1
+            step += 1
             print(step)
             max_delta = -1
             for fir_loc in range(CarCompany.MAX_CARS + 1):
@@ -108,37 +114,37 @@ class CarCompany():
             if policy_stable:
                 break
 
+
 def figure_4_2():
     carcomp = CarCompany()
-    plt.figure(figsize=(40,40))
+    plt.figure(figsize=(40, 20))
     ite = 1
     while True:
-        ax = plt.subplot(2,3,ite)
-        fig = seaborn.heatmap(np.flipud(carcomp.policy))
+        ax = plt.subplot(2, 3, ite)
+        fig = seaborn.heatmap(np.flipud(carcomp.policy),cmap="YlGnBu")
         fig.set_title("Policy_{}".format(ite))
         fig.set_xlabel("#Cars at second location")
         fig.set_ylabel("#Cars at first location")
-        fig.set_yticks(list(reversed(range(CarCompany.MAX_CARS+1))))
+        fig.set_yticks(list(reversed(range(CarCompany.MAX_CARS + 1))))
         print("evaluation...")
         carcomp.policy_evaluation()
         print("extraction...")
         policy_stable = carcomp.policy_improvement()
-        print("stable",policy_stable)
+        print("stable", policy_stable)
         if policy_stable:
-            ax = plt.subplot(2,3,6,projection="3d")
-            x = np.arange(CarCompany.MAX_CARS+1)
-            y = np.arange(CarCompany.MAX_CARS+1)
-            x,y = np.meshgrid(x,y)
-            ax.plot_surface(x,y,CarCompany.state_values,cmap=cm.coolwarm)
+            ax = plt.subplot(2, 3, 6, projection="3d")
+            x = np.arange(CarCompany.MAX_CARS + 1)
+            y = np.arange(CarCompany.MAX_CARS + 1)
+            x, y = np.meshgrid(x, y)
+            ax.plot_surface(x, y, carcomp.state_values, cmap=cm.coolwarm)
             ax.set_xlabel("#Cars at second location")
             ax.set_ylabel("#Cars at first location")
             ax.set_title("Optimal state value")
             break
-        ite+=1
+        ite += 1
     plt.savefig("Figure_4_2.png")
     plt.close()
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     figure_4_2()
