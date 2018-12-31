302b1f5fd797f5ff39cd2cfcdf64f146357315a8
sunnyyeti <sunnyyetizh@gmail.com>
2018-12-31 10:17:22 +0100

    hehe

--- Chapter4/Jack_car_rental.py
+++ Chapter4/Jack_car_rental.py
@@ -2,4 +2,8 @@ import numpy as np
 import seaborn
 import matplotlib.pyplot as plt
 
-class
+class CarCompany():
+    MAX_CARS = 20
+    ACTIONS = np.arange(-5,6)
+    def __init__(self):
+        self.state_values = np.zeros((CarCompany.MAX_CARS+1,CarCompany.MAX_CARS+1),dtype=np.float32)
