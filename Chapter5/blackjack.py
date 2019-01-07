f0aadd4673143a3580a99dcdf682137959544b95
sunnyyeti <sunnyyetizh@gmail.com>
2019-01-07 16:28:19 +0100

    figure_5_2_bug fixed

--- Chapter5/blackjack.py
+++ Chapter5/blackjack.py
@@ -5,6 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import seaborn
 
+
 class Card:
     CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
@@ -16,6 +17,25 @@ class Card:
 class Policy():
     def __init__(self, map_table):
         self.map_table = map_table
+
+
+class Player_Policy(Policy):
+    def __init__(self, map_table):
+        super(Player_Policy, self).__init__(map_table)
+
+    def __call__(self, state):
+        return self.map_table[state[0] - 12, state[1] - 1, state[2]]
+
+    def update_policy(self, map_table):
+        self.map_table = map_table
+
+
+class Dealer_Policy(Policy):
+    def __init__(self, map_table):
+        super(Dealer_Policy, self).__init__(map_table)
+
+    def __call__(self, state):
+        return self.map_table[state[0]]
 
 
 class Participant:
@@ -39,7 +59,11 @@ class Participant:
             if self.sum + 11 > Participant.MAX:
                 self.sum += 1
                 if self.sum > Participant.MAX:
-                    self.burst = True
+                    if not self.usable_ace:
+                        self.burst = True
+                    else:
+                        self.sum -= 10
+                        self.usable_ace = False
             else:
                 self.sum += 11
                 self.usable_ace = True
@@ -52,8 +76,6 @@ class Participant:
                     self.usable_ace = False
             else:
                 self.sum += card
-
-
 
 
 class Player(Participant):
@@ -93,6 +115,7 @@ class Dealer(Participant):
     def __init__(self):
         super(Dealer, self).__init__()
         self.show = None
+
     def init(self, first_card):
         self.show = first_card
         if first_card == 1:
@@ -120,7 +143,6 @@ class Dealer(Participant):
                 act = self.policy(self.state)
 
 
-
 class Blackjack:
     def __init__(self):
         self.player = Player()
@@ -135,9 +157,9 @@ class Blackjack:
             def __call__(self, state):
                 return self.map_table[state[0]]
 
-        player_policy = np.ones(Participant.MAX + 1,dtype=np.int8)
+        player_policy = np.ones(Participant.MAX + 1, dtype=np.int8)
         player_policy[20] = player_policy[21] = Participant.STICK
-        dealer_policy = np.ones(Participant.MAX + 1,dtype=np.int8)
+        dealer_policy = np.ones(Participant.MAX + 1, dtype=np.int8)
         for i in range(17, Participant.MAX + 1):
             dealer_policy[i] = Participant.STICK
 
@@ -147,7 +169,8 @@ class Blackjack:
         show_cards = list(range(1, 11))
         usable_aces = [True, False]
         state_values = {True: np.zeros((10, 10), dtype=np.float64), False: np.zeros((10, 10), dtype=np.float64)}
-        state_cnts = {True: np.zeros((10, 10), dtype=np.float64) + 1e-12, False: np.zeros((10, 10), dtype=np.float64)}
+        state_cnts = {True: np.zeros((10, 10), dtype=np.float64) + 1e-12,
+                      False: np.zeros((10, 10), dtype=np.float64) + 1e-12}
         for _ in tqdm(range(episodes)):
             play_sum = np.random.choice(cur_sums)
             dealer_show = np.random.choice(show_cards)
@@ -166,37 +189,20 @@ class Blackjack:
                 else:
                     reward = 0
             for player_sum, dealer_show, player_useace, _ in player_trajectory:
-                state_values[player_useace][play_sum - 12][dealer_show - 1] += reward
-                state_cnts[player_useace][play_sum - 12][dealer_show - 1] += 1.0
+                state_values[player_useace][player_sum - 12][dealer_show - 1] += reward
+                state_cnts[player_useace][player_sum - 12][dealer_show - 1] += 1.0
         state_values[True] = state_values[True] / state_cnts[True]
         state_values[False] = state_values[False] / state_cnts[False]
         return state_values
 
-    def action_value_estimation_with_exploring_states(self,episodes):
-        class Player_Policy(Policy):
-            def __init__(self, map_table):
-                super(Player_Policy, self).__init__(map_table)
-
-            def __call__(self, state):
-                return self.map_table[state[0]-12, state[1]-1, state[2]]
-
-            def update_policy(self, map_table):
-                self.map_table = map_table
-
-        class Dealer_Policy(Policy):
-            def __init__(self, map_table):
-                super(Dealer_Policy, self).__init__(map_table)
-
-            def __call__(self, state):
-                return self.map_table[state[0]]
-
-        dealer_policy = np.ones(Participant.MAX + 1,dtype=np.int8)
+    def action_value_estimation_with_exploring_states(self, episodes):
+        dealer_policy = np.ones(Participant.MAX + 1, dtype=np.int8)
         for i in range(17, Participant.MAX + 1):
             dealer_policy[i] = Participant.STICK
         dealer_policy = Dealer_Policy(dealer_policy)
         self.dealer.set_policy(dealer_policy)
-        player_policy = np.ones((10,10,2),dtype=np.int8) ##sum(12-21), dealer_show(1-10), usable_ace
-        player_policy[-2:,:,:] = Participant.STICK
+        player_policy = np.ones((10, 10, 2), dtype=np.int8)  ##sum(12-21), dealer_show(1-10), usable_ace
+        player_policy[-2:, :, :] = Participant.STICK
         player_policy = Player_Policy(player_policy)
         self.player.set_policy(player_policy)
         action_values = np.zeros((10, 10, 2, 2), dtype=np.float64)  ##sum(12-21), dealer_show(1-10), usable_ace, action
@@ -204,7 +210,7 @@ class Blackjack:
         cur_sums = list(range(12, 22))
         show_cards = list(range(1, 11))
         usable_aces = [True, False]
-        actions = [Participant.HIT,Participant.STICK]
+        actions = [Participant.HIT, Participant.STICK]
         for _ in tqdm(range(episodes)):
             play_sum = np.random.choice(cur_sums)
             use_ace = np.random.choice(usable_aces)
@@ -223,42 +229,75 @@ class Blackjack:
                     reward = -1
                 else:
                     reward = 0
-            for player_sum, dealer_show, player_useace, act in player_trajectory:
-                action_values[play_sum-12,dealer_show-1,player_useace,act]+=reward
-                action_counts[play_sum-12,dealer_show-1,player_useace,act]+=1.0
-            action_ave_reward = action_values/action_counts
+            for player_sum, dealer_card, player_useace, act in player_trajectory:
+                action_values[player_sum - 12, dealer_card - 1, player_useace, act] += reward
+                action_counts[player_sum - 12, dealer_card - 1, player_useace, act] += 1.0
+            action_ave_reward = action_values / action_counts
             new_policy = action_ave_reward.argmax(axis=-1)
-            # new_policy = np.zeros_like(player_policy.map_table)
-            # for i in range(10):
-            #     for j in range(10):
-            #         for k in range(2):
-            #             values = action_ave_reward[i,j,k,:]
-            #             action = np.random.choice([ a for a,v in enumerate(values) if v==np.max(values)])
-            #             new_policy[i,j,k]=action
             player_policy.update_policy(new_policy)
         return action_ave_reward
+
+    def off_policy_fixed_state_value_estimation(self, episodes, method):
+        behaviour_poliy = lambda _: np.random.choice([Participant.HIT, Participant.STICK])
+        dealer_policy = np.ones(Participant.MAX + 1, dtype=np.int8)
+        for i in range(17, Participant.MAX + 1):
+            dealer_policy[i] = Participant.STICK
+        dealer_policy = Dealer_Policy(dealer_policy)
+        self.dealer.set_policy(dealer_policy)
+        target_policy = np.ones((10, 10, 2), dtype=np.int8)  ##sum(12-21), dealer_show(1-10), usable_ace
+        target_policy[-2:, :, :] = Participant.STICK
+        target_policy = Player_Policy(target_policy)
+        self.player.set_policy(behaviour_poliy)
+        PLAYER_SUM = 13
+        PLAYER_USABLE_ACE = True
+        DEALER_SHOW = 2
+        state_values = np.zeros(episodes,dtype=np.float32)
+        state_accured_scaled_reward = 0.0
+        state_accured_weights = 1e-12
+        for episode in range(episodes):
+            self.player.init(PLAYER_SUM,PLAYER_USABLE_ACE,None)
+            self.dealer.init(DEALER_SHOW)
+            player_trajectory = self.player.run()
+            if self.player.burst:
+                reward = -1
+            else:
+                self.dealer.run()
+                if self.dealer.burst or self.player.sum > self.dealer.sum:
+                    reward = 1
+                elif self.player.sum < self.dealer.sum:
+                    reward = -1
+                else:
+                    reward = 0
+            scale = 1.0
+            for player_sum, dealer_card, player_useace, act in player_trajectory:
+                if act==target_policy((player_sum,dealer_card,player_useace)):
+                    scale*=2.0
+                else:
+                    scale=0.0
+                    break
+            state_accured_scaled_reward+=scale*reward
+            if method=="ordinary":
+                state_accured_weights+=1.0
+            else:
+                state_accured_weights+=scale
+            state_values[episode] = state_accured_scaled_reward/state_accured_weights
+        return state_values
+
+
+
 
 
 
 def figure_5_1():
-    # def draw(i, value, xlabel, ylabel, title):
-    #     ax = plt.subplot(2, 2, i)
-    #     policy = np.flipud(value)
-    #     fig = seaborn.heatmap(policy,cbar=True,cmap="YlGnBu",xticklabels=range(1, 11),
-    #                       yticklabels=list(reversed(range(12, 22))))
-    #     fig.set_xlabel(xlabel)
-    #     fig.set_ylabel(ylabel)
-    #     fig.set_title(title)
-
     def draw(i, value, xlabel, ylabel, title):
         ax = plt.subplot(2, 2, i, projection="3d")
         x = np.arange(1, 11)
         y = np.arange(12, 22)
         x, y = np.meshgrid(x, y)
         ax.plot_surface(x, y, value, cmap=cm.coolwarm)
-        ax.set_xlabel(xlabel)
-        ax.set_ylabel(ylabel)
-        ax.set_title(title)
+        ax.set_xlabel(xlabel, fontsize=30)
+        ax.set_ylabel(ylabel, fontsize=30)
+        ax.set_title(title, fontsize=30)
 
     blackjack = Blackjack()
     plt.figure(figsize=(40, 30))
@@ -274,37 +313,70 @@ def figure_5_1():
     plt.savefig("Figure_5_1.png")
     plt.close()
 
+
 def figure_5_2():
-    def draw_policy_heatmap(i,policy,xlabel,ylabel,title):
-        ax=plt.subplot(2,2,i)
+    def draw_policy_heatmap(i, policy, xlabel, ylabel, title):
+        plt.subplot(2, 2, i)
         policy = np.flipud(policy)
-        fig = seaborn.heatmap(policy,cbar=True,cmap="YlGnBu",xticklabels=range(1, 11),
-                          yticklabels=list(reversed(range(12, 22))))
-        fig.set_xlabel(xlabel)
-        fig.set_ylabel(ylabel)
-        fig.set_title(title)
-    def draw_state_value(i,statevalue,xlabel,ylabel,title):
+        fig = seaborn.heatmap(policy, cbar=True, cmap="YlGnBu", xticklabels=range(1, 11),
+                              yticklabels=list(reversed(range(12, 22))))
+        fig.set_xlabel(xlabel, fontsize=30)
+        fig.set_ylabel(ylabel, fontsize=30)
+        fig.set_title(title, fontsize=30)
+
+    def draw_state_value(i, statevalue, xlabel, ylabel, title):
         ax = plt.subplot(2, 2, i, projection="3d")
         x = np.arange(1, 11)
         y = np.arange(12, 22)
         x, y = np.meshgrid(x, y)
         ax.plot_surface(x, y, statevalue, cmap=cm.coolwarm)
-        ax.set_xlabel(xlabel)
-        ax.set_ylabel(ylabel)
-        ax.set_title(title)
+        ax.set_xlabel(xlabel, fontsize=30)
+        ax.set_ylabel(ylabel, fontsize=30)
+        ax.set_title(title, fontsize=30)
 
     blackjack = Blackjack()
-    plt.figure(figsize=(40,30))
+    plt.figure(figsize=(40, 30))
     plt.subplots_adjust(wspace=0.1, hspace=0.2)
-    action_ave_values = blackjack.action_value_estimation_with_exploring_states(1000000)##sum, show, usable_ace, act
-    usable_ace_action_values = action_ave_values[:,:,1,:]
-    no_usable_ace_action_values = action_ave_values[:,:,0,:]
-    draw_policy_heatmap(1,np.argmax(usable_ace_action_values,axis=-1),"Dealer showing","Player sum","Usable ace; Policy")
-    draw_policy_heatmap(3,np.argmax(no_usable_ace_action_values,axis=-1),"Dealer showing","Player sum","No usable ace; Policy")
-    draw_state_value(2,usable_ace_action_values.max(axis=-1),"Dealer showing","Player sum","Usable ace; State value")
-    draw_state_value(4,no_usable_ace_action_values.max(axis=-1),"Dealer showing","Player sum","No usable ace; State value")
+    action_ave_values = blackjack.action_value_estimation_with_exploring_states(1000000)  ##sum, show, usable_ace, act
+    usable_ace_action_values = action_ave_values[:, :, 1, :]
+    no_usable_ace_action_values = action_ave_values[:, :, 0, :]
+    draw_policy_heatmap(1, np.argmax(usable_ace_action_values, axis=-1), "Dealer showing", "Player sum",
+                        "Usable ace; Policy")
+    draw_policy_heatmap(3, np.argmax(no_usable_ace_action_values, axis=-1), "Dealer showing", "Player sum",
+                        "No usable ace; Policy")
+    draw_state_value(2, usable_ace_action_values.max(axis=-1), "Dealer showing", "Player sum",
+                     "Usable ace; State value")
+    draw_state_value(4, no_usable_ace_action_values.max(axis=-1), "Dealer showing", "Player sum",
+                     "No usable ace; State value")
     plt.savefig("Figure_5_2.png")
     plt.close()
+
+def figure_5_3():
+    blackjack = Blackjack()
+    runs = 100
+    episodes = 10000
+    state_values = np.zeros((2,runs,episodes),dtype=np.float32)
+    methods = ["ordinary","weighted"]
+    baseline = -0.27726
+    for m,method in enumerate(methods):
+        for run in tqdm(range(runs)):
+            state_vs = blackjack.off_policy_fixed_state_value_estimation(episodes,method)
+            state_values[m,run,:] = state_vs
+    ordinary = np.power(state_values[0]-baseline,2).mean(axis=0)
+    weighted = np.power(state_values[1]-baseline,2).mean(axis=0)
+    plt.figure()
+    plt.plot(np.arange(1,episodes+1),ordinary,label="Ordinary importance sampling")
+    plt.plot(np.arange(1,episodes+1),weighted,label="Weighted importance sampling")
+    plt.xscale("log")
+    plt.ylabel("Mean squared error (average over {} runs)".format(runs))
+    plt.xlabel("Episodes (log scale)")
+    plt.legend()
+    plt.grid()
+    plt.savefig("Figure_5_3.png")
+    plt.close()
+
+
 if __name__ == "__main__":
-    #figure_5_1()
-    figure_5_2()
+    # figure_5_1()
+    #figure_5_2()
+    figure_5_3()
